3fb19b5b9d06318f5095a382d5fe8adb7bcbb96a
bhaktbivaradai <bhaktibasavaraj.cs24@bmsce.ac.in>
2026-03-25 12:11:54 +0530

    Add files via upload

---
+++ multilevel_queue_lab4.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#define MAX 100
+int main() {
+    int n;
+    int PID[MAX],AT[MAX],BT[MAX],TYPE[MAX];
+    int CT[MAX],TAT[MAX],WT[MAX],Start[MAX];
+    int systemQueue[MAX], userQueue[MAX];
+    int sysCount=0, userCount=0;
+    int time=0;
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+    for (int i=0;i<n;i++) {
+        printf("\nProcess %d\n", i+1);
+        printf("PID: ");
+        scanf("%d",&PID[i]);
+        printf("Arrival Time: ");
+        scanf("%d",&AT[i]);
+        printf("Burst Time: ");
+        scanf("%d",&BT[i]);
+        printf("Type (0=System,1=User): ");
+        scanf("%d",&TYPE[i]);
+    }
+    for (int i=0;i<n;i++) {
+        if (TYPE[i]==0) {
+            systemQueue[sysCount++]=i;
+        } else {
+            userQueue[userCount++]=i;
+        }
+    }
+    for (int i=0;i<sysCount-1;i++) {
+        for (int j=i+1;j<sysCount;j++) {
+            if (AT[systemQueue[i]]>AT[systemQueue[j]]) {
+                int temp=systemQueue[i];
+                systemQueue[i]=systemQueue[j];
+                systemQueue[j]=temp;
+            }
+        }
+    }
+    for (int i=0;i<userCount-1;i++) {
+        for (int j=i+1;j<userCount;j++) {
+            if (AT[userQueue[i]] > AT[userQueue[j]]) {
+                int temp=userQueue[i];
+                userQueue[i]=userQueue[j];
+                userQueue[j]=temp;
+            }
+        }
+    }
+    for (int i=0;i<sysCount;i++) {
+        int p = systemQueue[i];
+
+        if (time<AT[p])
+            time=AT[p];
+
+        Start[p]=time;
+        time+=BT[p];
+        CT[p]=time;
+
+        TAT[p]=CT[p]-AT[p];
+        WT[p]=TAT[p]-BT[p];
+    }
+    for (int i=0;i<userCount;i++) {
+        int p = userQueue[i];
+
+        if (time<AT[p])
+            time=AT[p];
+
+        Start[p]=time;
+        time+=BT[p];
+        CT[p]=time;
+
+        TAT[p]=CT[p]-AT[p];
+        WT[p]=TAT[p]-BT[p];
+    }
+    printf("\n\nPID\tAT\tBT\tTYPE\tCT\tTAT\tWT\n");
+    for (int i=0;i<n;i++) {
+        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+               PID[i],AT[i],BT[i],TYPE[i],
+               CT[i],TAT[i],WT[i]);
+    }
+    return 0;
+}
